c00f0b22f51a7db5b67f8d3b4122628d498edbc0
Javan <159927475+Vansaher@users.noreply.github.com>
2024-09-05 00:47:48 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
 import random
 import art
 
-print(art.logo)
-print("Welcome to the Number Guessing Game!")
+
+def get_num():
+    return random.randrange(1, 101)
 
 
-num = random.randrange(1, 100)
-
-diff = input("Choose a difficulty. Type 'easy or 'hard': ")
-if diff == "hard":
-    tries = 5
-else:
-    tries = 10
-
-while tries != 0:
-    print(f"You have {str(tries)} attempts remaining to guess the number.")
-    guess = int(input("Make a guess: "))
-    if guess != num:
-        if guess < num:
-            print("Too low")
-        elif guess > num:
-            print("Too high")
-        tries -= 1
+def check_guess(guess, answer):
+    if guess < answer:
+        print("Too low")
+        return False
+    elif guess > answer:
+        print("Too high")
+        return False
     else:
-        print("That correct! Congrats")
-        tries = 0
-
-print("Game Over")
+        print("That's correct! Congrats")
+        return True
 
 
+def replay():
+    re = input("Do you want to play again?").lower()
+    if re == "yes":
+        play_game()
+    else:
+        print("Goodbye")
+
+
+def play_game():
+    print(art.logo)
+    print("Welcome to the Number Guessing Game!")
+
+    answer = get_num()
+    diff = input("Choose a difficulty. Type 'easy or 'hard': ").lower()
+    if diff == "hard":
+        tries = 5
+    else:
+        tries = 10
+
+    while tries > 0:
+        print(f"You have {tries} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+
+        if check_guess(guess, answer):
+            break  # Exit the loop if the guess is correct
+
+        tries -= 1  # Decrement tries after each guess
+
+    if tries == 0:
+        print(f"Game Over. The correct number was {answer}.")
+
+    replay()
+
+
+play_game()
